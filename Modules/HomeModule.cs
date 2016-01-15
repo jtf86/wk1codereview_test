f5475899d21f3a06d9052e9dfb4e59af50e3e85c
John <john@epicodus.com>
2016-01-15 10:29:42 -0800

    frame basic HomeModule.cs file

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using Nancy.ViewEngines.Razor;
+using System.Collections.Generic;
+
+namespace ContactList
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      //routes go here
+    }
+  }
+}
